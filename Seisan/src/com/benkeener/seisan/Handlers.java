b471171ad9d89ed60ceca5ef880ec26164c4d0aa
Ben <Ben@192.168.0.23>
2018-11-27 17:26:09 -0500

    Added use of handlers for requests to make room for API

--- Seisan/src/com/benkeener/seisan/Handlers.java
+++ Seisan/src/com/benkeener/seisan/Handlers.java
@@ -1,7 +1,9 @@
 package com.benkeener.seisan;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-
+import java.net.URI;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -11,11 +13,34 @@ public class Handlers {
 
 		@Override
 		public void handle(HttpExchange exc) throws IOException {
-			String res = "Success";
-			exc.sendResponseHeaders(200, res.length());
-			OutputStream out = exc.getResponseBody();
-			out.write(res.getBytes());
-			out.close();
+			String root = "./../static/";
+			URI uri = exc.getRequestURI();
+			File file = new File(root + uri.getPath()).getCanonicalFile();
+			if (uri.toString().equals("/")) {
+				file = new File(root + "/index.html").getCanonicalFile();
+			}
+			
+			System.out.println(file.getPath());
+			
+			if (!file.isFile()) {
+				//File does not exist
+				String res = "404 File Not Found";
+				exc.sendResponseHeaders(404, res.length());
+				OutputStream out = exc.getResponseBody();
+				out.write(res.getBytes());
+				out.close();
+			} else {
+				exc.sendResponseHeaders(200, 0);
+				OutputStream out = exc.getResponseBody();
+				FileInputStream fs = new FileInputStream(file);
+				final byte[] buffer = new byte[0x10000];
+				int i = 0;
+				while ((i = fs.read(buffer)) >= 0) {
+					out.write(buffer, 0, i);
+				}
+				fs.close();
+				out.close();
+			}
 		}
 		
 	}

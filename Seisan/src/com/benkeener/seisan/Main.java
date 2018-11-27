1b385e34a2d4f0e536124d3930c274f1939aa6aa
Ben <Ben@192.168.0.23>
2018-11-27 18:56:16 -0500

    Added JDBC driver and basic DB interaction capabilities

--- Seisan/src/com/benkeener/seisan/Main.java
+++ Seisan/src/com/benkeener/seisan/Main.java
@@ -4,8 +4,11 @@ import com.benkeener.seisan.Server;
 public class Main {
 	public static int port = 80;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Server httpServer = new Server();
 		httpServer.Start(port);
+		
+		MySQLHandler sql = new MySQLHandler();
+		sql.loadDataBase();
 	}
 }
---
+++ Seisan/src/com/benkeener/seisan/MySQLHandler.java
@@ -0,0 +1,56 @@
+package com.benkeener.seisan;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MySQLHandler {
+	private Connection connection = null;
+	private Statement statement = null;
+	private ResultSet resultSet = null;
+	
+	private PreparedStatement addFoo = null;
+	
+	public void loadDataBase() throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		connection = DriverManager.getConnection("jdbc:mysql://localhost/jtest?user=ben&password=password");
+		
+		statement = connection.createStatement();
+		
+		addFoo = connection.prepareStatement("INSERT INTO foo VALUES (default, ?, ?)");
+		addFoo.setString(1, "bar");
+		addFoo.setInt(2, 32);
+		addFoo.executeUpdate();
+		
+		resultSet = statement.executeQuery("SELECT * FROM foo");
+		parseMeta(resultSet);
+		parseData(resultSet);
+		
+	}
+	
+	private void parseMeta(ResultSet resultSet) throws Exception {
+
+        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+        for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
+            System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
+        }
+        
+    }
+	
+	private void parseData(ResultSet resultSet) throws Exception {
+		
+		while (resultSet.next()) {
+			int id = resultSet.getInt("ID");
+			String name = resultSet.getString("name");
+			int age = resultSet.getInt("age");
+			System.out.println("ID: " + String.valueOf(id));
+			System.out.println("Name: " + name);
+			System.out.println("Age: " + String.valueOf(age));
+		}
+		
+	}
+}
